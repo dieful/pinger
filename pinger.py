e0c3aa8fc6d35de428deafcbf49b64cdc02d209c
elf <126424282+dieful@users.noreply.github.com>
2023-03-06 18:35:39 -0800

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -1,19 +1,20 @@
 import os
+import re
 from time import sleep
 from colorama import Fore
 
 
-#CHOOSE COLOR YOU WANT (RED, YELLOW, GREEN, BLUE, CYAN, MAGENTA)
+# CHOOSE COLOR YOU WANT (RED, YELLOW, GREEN, BLUE, CYAN, MAGENTA)
 color = 'BLUE' 
 
-#Set to 'true' if you want the light version of your selected color
+# Set to 'true' if you want the light version of your selected color
 lightcolor = 'false' 
 
-#Set to what you want the titlebar to say
+# Set to what you want the titlebar to say
 title = 'IP Pinger'  
 
-#https://patorjk.com/software/taag/  - This site is used to make 3d text 
-#https://www.asciiart.eu - ascii artwork
+# https://patorjk.com/software/taag/  - This site is used to make 3d text 
+# https://www.asciiart.eu - ascii artwork
 bannertext = """
 ██████╗ ██╗███╗   ██╗ ██████╗ ███████╗██████╗  
 ██╔══██╗██║████╗  ██║██╔════╝ ██╔════╝██╔══██╗ 
@@ -23,23 +24,34 @@ bannertext = """
 ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
 """
 
-
+def is_valid_ipv4_address(ip_address):
+    pattern = re.compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+    return pattern.match(ip_address) is not None
 
 
 def main():
     os.system('cls||clear')
     os.system(f'title {title}')
     a = getattr(Fore, color)
-    if lightcolor == 'true': a = getattr(Fore, f"LIGHT{color}_EX")
-    else:a = getattr(Fore, color)
+    if lightcolor == 'true':
+        a = getattr(Fore, f"LIGHT{color}_EX")
+    else:
+        a = getattr(Fore, color)
     print(f"""{a}{bannertext}{Fore.RESET} """)
-    
+
     hostname = input(f"[ {a}+{Fore.RESET} ] IP Address: ")
+    if not is_valid_ipv4_address(hostname):
+        print(f"[ {Fore.RED}-{Fore.RESET} ] Invalid IP address")
+        sleep(2)
+        main()
+
     while True:
         try:
-         print(a)
-         response = os.system("ping " + hostname + ' -t')
-        except: 
-           main()
-            
+            print(a)
+            response = os.system("ping " + hostname + ' -t')
+        except Exception as e:
+            print(f"[ {Fore.RED}-{Fore.RESET} ] Ping failed: {e}")
+            sleep(2)
+            main()
+
 main()
